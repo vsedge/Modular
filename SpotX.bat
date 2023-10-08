52fe5e53f8fe8fb10d057232721ff1d796acb0a2
vsedge <115322985+vsedge@users.noreply.github.com>
2023-10-08 19:51:08 +0200

    Update SpotX.bat

--- SpotX.bat
+++ SpotX.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -new_theme -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti """" | Invoke-Expression"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -new_theme -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -adsections_off -lyrics_stat spotify """" | Invoke-Expression"
 
 exit /b
 
